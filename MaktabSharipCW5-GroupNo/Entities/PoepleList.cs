8c328fb8307c34193ba5264ae4ebe19b3b76439e
Hossein913 <Hosssein913@gmail.com>
2023-05-18 16:26:35 +0330

    Add Entities Folder to project and add PeopleList class on it

---
+++ MaktabSharipCW5-GroupNo/Entities/PoepleList.cs
@@ -0,0 +1,14 @@
+﻿using MaktabSharipCW5_GroupNo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaktabSharipCW5_GroupNo.Entities
+{
+    class PoepleList
+    {
+        List<Person> People = new List<Person>();
+    }
+}
